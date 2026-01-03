18310846fd016bf19645e3ac03fb483bedb83376
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-03 14:26:43 +0545

    learning variables and keywords

--- Day_1/firstprogram.py
+++ Day_1/firstprogram.py
@@ -1,7 +1,15 @@
 name="Aaryan Ghimire"
 age=19
 Level="Bachelors"
+Masters=False
+Backs=None
 
 print("My name is:",name)
 print("My age is:",age)
 print("My level is:",Level)
+
+print(type(name))
+print(type(age))
+print(type(Level))
+print(type(Masters))
+print(type(Backs))
