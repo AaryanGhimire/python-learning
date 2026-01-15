e5c0e045aa6eaefc459451d7a988441c9c71e98c
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-15 13:18:29 +0545

    Single line conditional statements

--- Day_2/Conditionalstmnt.py
+++ Day_2/Conditionalstmnt.py
@@ -15,3 +15,13 @@ elif(light=="green"):
     print("go")
 else:
     print("The light is broken")
+
+#single line conditional statements
+
+food=input("food:")
+eat="yes" if food=="cake" else "no"
+print(eat)
+
+#another format
+food=input("food:")
+print("sweet") if food=="cake" or food=="jalebi" else print("not sweet")
