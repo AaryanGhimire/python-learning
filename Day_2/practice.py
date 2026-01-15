ac5596a5176a7f2bc25c1395cfa90be6ada0a8eb
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-15 14:07:51 +0545

    Practiced some program related to day2

---
+++ Day_2/practice.py
@@ -0,0 +1,18 @@
+#Wap to calc sum of two numbers taken from a user
+a=int(input(" enter first number:"))
+b=int(input("enter second number:"))
+print("sum=",a+b)
+
+#Wap to input side of a square and print its area
+side=float(input("Input the side of square:"))
+print("area=",side*side)
+
+#Wap to input 2 floatiung number and calc their average
+a=float(input(" enter first number:"))
+b=float(input("enter second number:"))
+print("avg",(a+b)/2)
+
+#Wap to input 2 int numbers a and b. print true if a greater than b or equal to b . if not print false
+a=int(input(" enter first number:"))
+b=int(input("enter second number:"))
+print(a>=b)
