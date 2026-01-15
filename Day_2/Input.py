27c348c8677be447c8743059e1051ecbd4b71151
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-15 12:37:47 +0545

    using Input statements

---
+++ Day_2/Input.py
@@ -0,0 +1,12 @@
+# input() statement is used to accept values (using keyboard) from user
+
+name=input("name:")
+print(name)
+
+age=int(input("age:"))
+print(age)
+
+price=float(input("price:"))
+print(price)
+
+print("My name is ",name,"And my age is",age)
