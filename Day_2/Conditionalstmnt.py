94e64d8ce429780ff80f407ea81843e5ee8433f5
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-15 13:09:08 +0545

    Some conditional statements program

---
+++ Day_2/Conditionalstmnt.py
@@ -0,0 +1,17 @@
+age=int(input("age:"))
+if(age>=18 and age<=59):
+    print("Eligible for voting")
+else:
+    print("Not eligible for voting")
+
+#Conditional statements consits of Indentation ie if paxi ko arko line ma 4 spaces xodna parxa format milna parxa
+
+light=input("light color:")
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("go")
+else:
+    print("The light is broken")
