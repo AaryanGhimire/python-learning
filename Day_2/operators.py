2a7c7bfc05dc2f44162ec8053c60fd36dee0a494
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-15 13:45:43 +0545

    familiarization with operators in python

---
+++ Day_2/operators.py
@@ -0,0 +1,40 @@
+#arithematic operators
+a=7
+b=3
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b) # it is a to the power b ie a^b
+
+#Relational/comparision operator
+ 
+a=50
+b=40
+print(a==b) #output will be false
+print(a!=b)#output will be true
+print(a>=b)
+print(a>b)
+print(a<b)
+
+#assignment operators
+num = 10
+num+=10 #num =num(10) +10
+print("num:",num)
+num*=3#num=num(20)*3 
+#similarly like others -= /= %= **= are assignment operators
+
+#Logical operators(Not,And,OR)
+print(not False)
+print(not True)
+a=50 
+b=20
+print(not (a>b))# answer is not true ie false
+
+val1=True
+val2=False
+print("and operator",val1 and val2)#False
+print("or operator",val1 or val2)#True
+
+
