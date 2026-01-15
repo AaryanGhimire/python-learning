21598cbd983882ca5af63cfa60b5211dbf92e45d
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-15 13:54:23 +0545

    Learning type casting and type conversion

---
+++ Day_2/TypeConversion.py
@@ -0,0 +1,12 @@
+a=2
+b=4.25
+
+sum=a+b #python automatically converts 2 to 2.0
+print(sum)
+
+x="2"# this is a string
+y=float("2")#This is a float using typecasting
+print(type(x))
+print(type(y))
+print(x)
+print(y)
