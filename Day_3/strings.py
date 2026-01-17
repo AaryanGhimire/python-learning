66281be032edb241fbee18a4ac434f28e1128007
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-17 14:50:50 +0545

    Learning about strings

--- Day_3/strings.py
+++ Day_3/strings.py
@@ -2,3 +2,12 @@ str1="This is a string"
 str2='This is also a string'
 str3="""This is a string as well"""
 #Most of the times we use double quote but all three can be used
+
+str="tihs is a string .\n We are creating in python"
+print(str)
+#\n is a escsape squence and also \t
+
+str1="Aaryan"
+str2="Ghimire"
+finalstr=str1+str2 #string concatenation
+print(finalstr)
