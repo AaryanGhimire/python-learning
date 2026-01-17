f76da6b8eabd77bc3a348bd72b8fe4aaedda1345
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-17 15:50:32 +0545

    Practicing some questions

---
+++ Day_3/practice.py
@@ -0,0 +1,6 @@
+#WAP to input users first name and print its length
+name=input("Enter your first name:")
+print("Its length is:",len(name))
+#Wap to find the occurence of $ in the string
+string=input("Enter a string")
+print("The occurence of $ in the string is ",string.count("$"),"times")
