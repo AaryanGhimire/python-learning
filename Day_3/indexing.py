11b9c30d48dcb303c56724a0ba3d202419758f7d
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-17 14:59:21 +0545

    indexing in string

--- Day_3/indexing.py
+++ Day_3/indexing.py
@@ -2,4 +2,7 @@
 #index helps in accesing characters
 str="Aaryan Ghimire"
 ch=str[6]
-print(ch)  
+print(ch) 
+#or we can directly write
+print(str[4]) 
+#But we cannot assign string in any other index ie str[4]="@" is not possible
