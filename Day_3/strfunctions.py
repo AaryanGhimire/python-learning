1c14afe7a760a7307f68fe5d407a4e7528e546ca
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-17 15:39:27 +0545

    Learning about string functions

---
+++ Day_3/strfunctions.py
@@ -0,0 +1,8 @@
+str="i am studyiing python."
+print(str.endswith("on."))#true if true false if false
+print(str.capitalize())#capitalizes first character
+print(str.replace("python","javascript"))#replaces old value with new value
+print(str.find("o"))#searching index of the character
+print(str.count("i"))#counts the occurence of substring
+#There are many string functions which can be accesed using str.
+
