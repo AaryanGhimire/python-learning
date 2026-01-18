8bac90c6b927f8ea2df3585620a3a3fc92cd0cb7
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-18 17:29:24 +0545

    Learning about tuples

---
+++ Day_4/tuples.py
@@ -0,0 +1,9 @@
+#A built in data type that lets us create immutable sequences of values
+#list is mutable but strings and tuples are immutable
+#list ma square bracket[] tara tuple ma paranthesis()
+tup=(2,1,3,1)
+print(tup[0])
+print(tup[1])
+print(type(tup))
+#tup(2) is an integer whereas tup(2,) is a tuple
+#tuple similarly has slicing as well ie print(tuple[3:2])
