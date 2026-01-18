ac487499c9aa2b8c8f031e790ecd3cc6075e85c3
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-18 17:27:42 +0545

    Learning about slicing in list

---
+++ Day_4/listslicing.py
@@ -0,0 +1,7 @@
+#Similar to string slicing
+#list_name[starting index:Ending index]#Ending index is not included
+#Example:
+marks=[85,94,33,23,45]
+print(marks[1:3])
+#It also can be done in negative index
+print(marks[-3:-1])
