95f386576803ba2d8d4272eb7783f2c2f2a53e44
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-18 17:26:51 +0545

    learning about lists

---
+++ Day_4/lists.py
@@ -0,0 +1,11 @@
+#A built in data types that stores set of values
+#(sort of equivalent of array in other languages)
+marks=[92.3,66.4,32.4,76.5,66.6]
+print(marks)
+print(type(marks))# lists
+#It can store element of different datatypes in single list as well
+student=["doku",67.3,"male",23]
+print(student[0])
+
+#string-> immutable that means that index can be accesed but value can not be changed ie str[0]="@" not possible
+#lists->mutable that means that index can be accesed and the value can be changed as well student[0]="Ram" is possible
