b2357fa341cef880393059d85aa774f8e95f9fee
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-19 08:55:14 +0545

    Day5

---
+++ Day_5/Dictionary.py
@@ -0,0 +1,16 @@
+#usied to store data values in "key":value pairs
+#they are unordered ,mutable(changeable)&dont allow multiple keys
+info=  {
+    
+    "name":"aaryan",
+    "topic":33
+}
+print(info)
+print(type(info))
+print(info["name"])
+info["name"]="Haaland"#changes name in info dictionary ie overwrite
+print(info)
+
+null_dict ={}
+null_dict["name"]="Cherki"
+print(null_dict)
