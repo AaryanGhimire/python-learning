2157679ee21b420275308499b37113e49ead0549
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-19 09:03:05 +0545

    Learning about nested dictionary

---
+++ Day_5/NestedDict.py
@@ -0,0 +1,11 @@
+#Dictionary inside Dictionary
+student={
+    "name":"Jon snow",
+    "subjects": {
+        "phy":97,
+        "chem":98,
+        "math":95
+    }
+} 
+
+print(student["subjects"]["chem"])#nested dictionary
