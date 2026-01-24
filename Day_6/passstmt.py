e253245adab7f72279bac990413fdd1634676fd1
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-24 18:08:48 +0545

    learning about pass statement

---
+++ Day_6/passstmt.py
@@ -0,0 +1,7 @@
+#pass is a null statement where we just dont want to work inside the loop .It is used as a placeholder for future code
+
+for i in range(5):
+    pass
+if i>5:
+    pass
+print("some useful work")
