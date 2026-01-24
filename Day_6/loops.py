f77639a112c4eeda17809a662bf0b2608345bc66
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-24 18:07:47 +0545

    Learning about loops

---
+++ Day_6/loops.py
@@ -0,0 +1,14 @@
+#used to repeat instructions
+#two types of loops in python
+#1 while loops
+count=1
+while count<=5:
+    print("hello",count)
+    count +=1
+#program to count till 5
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+print("Loop ended")
