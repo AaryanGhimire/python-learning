0e23a785e5f5c4216caadc93a3f44963a7903c8f
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-01-25 17:06:29 +0545

    learning about functions

---
+++ Day_7/functions.py
@@ -0,0 +1,19 @@
+#blocks of statements that perform specific tasks
+#function definintions
+def calc_sum(a,b):#parameters
+    return a+b
+
+sum=calc_sum(2,3)#function call;arguements
+print(sum)
+#Another example if no return then output will be none 
+def print_hello():
+    print("Hello")
+
+output=print_hello()#output is hello
+print(output)#output is none 
+
+#avg of 3 numbers
+def avg(a,b,c):
+    return (a+b+c)/3
+average=avg(2,3,4)
+print(average)
