4d50db2fe0cda9fca04f23d2c193034b52942202
Aaryan Ghimire <ghimireaaryan5@gmail.com>
2026-04-24 11:23:37 +0545

    introduction to OOP

---
+++ Day_9/oop.py
@@ -0,0 +1,15 @@
+#creating class
+class Student:
+    name="Aaryan Ghimire"
+
+#creating object(instance)
+s1=Student()
+print(s1.name)
+
+class Car:
+    color="blue"
+    brand="bmw"
+
+car1=Car()
+print(car1.color)
+print(car1.brand)
